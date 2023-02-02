e2a6fa5094c77fa7e02199a8f6a779109508bee2
xin.zhu <xin.zhu@mxplayer.in>
2023-02-02 20:06:20 +0800

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zxintop/webapi
 
 go 1.18
+
+require github.com/zxintop/gocommon v0.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zxintop/gocommon/strutil"
+)
+
+func main() {
+	str := strutil.ToUpper("Hello, world")
+	fmt.Println(str)
+}
